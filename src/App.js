3b0c1c9a504ff0cfbfbe08b34ca1d0c196d09f83
Bereket Dereje <bdereje@wustl.edu>
2020-12-17 16:58:52 -0600

    small change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Todo from './Todo';
 
 // function App() {
-  class Todo extends Component (){
+  class App extends Component (){
     render(){
       return (
           // <div className="App">
